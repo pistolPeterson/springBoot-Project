2180fc9497aee5c519f12fcaab87e5e1bc8e0ce8
pistolPeterson <petersonnormil@gmail.com>
2021-12-16 19:54:13 -0500

    thymeleaf as a rendering engine(error)
    error when I try to compile

--- learning-spring/src/main/java/com/petersonnormil/lil/learningspring/LearningSpringApplication.java
+++ learning-spring/src/main/java/com/petersonnormil/lil/learningspring/LearningSpringApplication.java
@@ -21,43 +21,6 @@ public class LearningSpringApplication {
 		}
 		
 	//this is considered an antipattern, we are only doing this to show the data to the outside world
-		@RestController
-		@RequestMapping("/rooms")
-		public class RoomController{
-			@Autowired
-			private RoomRepository roomRepository;
-			
-			@GetMapping
-			public Iterable<Room> getRooms(){
-				return this.roomRepository.findAll();
-			}
-			
-		}
-		
-		@RestController
-		@RequestMapping("/guests")
-		public class GuestController{
-			@Autowired
-			private GuestRepository guestRepository;
-			
-			@GetMapping
-			public Iterable<Guest> getGuests(){
-				return this.guestRepository.findAll();
-			}
-		}
-		
-		@RestController
-		@RequestMapping("/reservations")
-		public class ReservationController{
-			@Autowired
-			private ReservationRepository reservationRepository;
-			
-			@GetMapping
-			public Iterable<Reservation> getGuests(){
-				return this.reservationRepository.findAll();
-			}
-		}
-		
-		
+	
 
 }
